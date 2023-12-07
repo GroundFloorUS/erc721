a6302287b261a5589df56bb8d4b176394c431414
Justin Burris <713795+justinbburris@users.noreply.github.com>
2023-12-07 11:34:31 -0500

    generate images when generating template

--- bin/generate_gfn_drop.mjs
+++ bin/generate_gfn_drop.mjs
@@ -1,24 +1,40 @@
 import { input } from '@inquirer/prompts';
-import { readFile, writeFile, mkdir } from 'fs/promises';
+import { copyFile, readFile, writeFile, mkdir } from 'fs/promises';
 import Handlebars from 'handlebars';
+import Jimp from "jimp";
+
+async function createPromissoryNote(imgPath, text) {
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+  const image = await Jimp.read(imgPath);
+
+  image.print(font, 1550, 1150, text);
+  await image.writeAsync(imgPath);
+}
 
 async function content(path) {  
   return await readFile(path, 'utf8');
 }
 
 // Ask the questions about the drop
-const series = await input({ message: 'What is the note series? (e.g. A,B,C,etc)' });
-const rate = parseFloat(await input({ message: 'What is the note rate? (e.g. 5.0)' }));
-const length = parseInt(await input({ message: 'What is the note length? (in days)' }));
-const maturityDate = await input({ message: 'What is the maturity date? (YYYY-MM-DD)' });
-const tokenCount = parseInt(await input({ message: 'How many tokens to be generated?' }));
+const series = await input({ message: 'What is the note series? (e.g. A,B,C,etc)', default: 'A' });
+const rate = parseFloat(await input({ message: 'What is the note rate? (e.g. 5.0)', default: 5.0 }));
+const length = parseInt(await input({ message: 'What is the note length? (in days)', default: 60 }));
+
+const defaultDate = new Date();
+defaultDate.setDate(defaultDate.getDate() + length);
+const maturityDateDefault = defaultDate.toISOString().split('T')[0]
+
+const maturityDate = await input({ message: 'What is the maturity date? (YYYY-MM-DD)', default: maturityDateDefault });
+
+const tokenCount = parseInt(await input({ message: 'How many tokens to be generated?', default: 10 }));
 
 const rootPath = `./tokens/groundfloor_note_token`;
 const seriesPath = `${rootPath}/drops/series-${series}`;
+const promissoryNoteTemplatePath = `${rootPath}/note-template.jpeg`;
 
-await mkdir(seriesPath, { recursive: true })
+await mkdir(seriesPath, { recursive: true });
 
-const template_source = await content(`${rootPath}/metadata_values.json.mustache`);
+const template_source = await content(`${rootPath}/metadata-values.json.mustache`);
 const template = Handlebars.compile(template_source);
 
 for (let tokenId = 0; tokenId < tokenCount; tokenId++) {
@@ -32,6 +48,11 @@ for (let tokenId = 0; tokenId < tokenCount; tokenId++) {
     tokenId
   }
   let fileName = `${seriesPath}/token-${tokenId}.json`;
+
+  let imgPath = `${seriesPath}/token-${tokenId}-promissory-note.jpeg` ;
+
+  await copyFile(promissoryNoteTemplatePath, imgPath);
+  createPromissoryNote(imgPath, `Series: ${tokenName}`);
 
   await writeFile(fileName, template(data));
 }
